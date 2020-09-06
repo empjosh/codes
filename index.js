fec9704b856fe1e96b6ccbb23b604ba0b382210c
Josh <empjoshkc@gmail.com>
2020-09-06 14:23:56 +0000

    The Nodes

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log('Working');
+
+// let arrCopy = [...arr];
+// myfunc = (input) => {
+//   if (toString.call(input) === '[object Array]') {
+//     return true;
+//   }
+//   return false;
+// };
+
+// let myColor = ['Red', 'Green', 'White', 'Black'];
+
+// let myfunction = function (input) {
+//   // return input.sort();
+//   // for (let i = 0; i < input.length - 1; i++) {
+//   //   if (input[i - 1] % 2 === 0 && input[i] % 2 === 0) {
+//   //     input.push('-', input[i]);
+//   //   } else {
+//   //     input.push(input[i]);
+//   //   }
+//   // }
+//   // return input.join('');
+//   //   return input[0];
+//   // return (input1 = [...input]);
+//   // return myColor.join(', ');
+// };
+
+// console.log(myfunction([1, 57, -12, 1, 35, -1131]));
+
+//////////////////////////////////////////////////////////////
+
+// function animal(name, age) {
+//   let animal = Object.create(animal.prototype);
+//   animal.name = name;
+//   animal.age = age;
+
+//   return animal;
+// }
+
+// animal.prototype.name = function () {
+//   return `The name of the animal is ` + this.name + `.`;
+// };
+// animal.prototype.age = function () {
+//   return `The age of the animal is ` + this.age + `.`;
+// };
+// animal.prototype.gender = function () {
+//   return `The gender of the animal is ` + this.gen + `.`;
+// };
+// animal.prototype.poop = function () {
+//   return `The animal does poop very much ` + this.poop + `.`;
+// };
+
+// const Pete = animal('Pete', 12);
+
+////////////////////////////////////////////////////////
+
+// const myObj = {
+//   name: 'Josh',
+//   Age: 22,
+//   Heritage: 'African',
+// };
+
+// const myObj1 = Object.create(myObj);
+// myObj1.name = 'Ryan';
+// myObj1.age = 12;
+
+// console.log(myObj1.name);
+// console.log(myObj1.age);
+// console.log(myObj1.Heritage);
